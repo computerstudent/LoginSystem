c3b48fe7744093cf69b31bc681392dcb01855de2
George <aeliusdev@users.noreply.github.com>
2016-01-11 08:54:23 +0000

    Added LoginUser and LogoutUser method

--- Login.cs
+++ Login.cs
@@ -6,4 +6,20 @@ public class Login ()
 	{
 		userLoggedIn = false;
 	}
+	
+	public void LoginUser (int id, string password)
+	{
+		if (id == this.id)
+		{
+			if (decryptPassword(password))
+			{
+				userLoggedIn = true;
+			}
+		}
+	}
+	
+	public void LogoutUser ()
+	{
+		userLoggedIn = false;
+	}
 }
